3c3db6a93f717877731d4814c266e8f7b6d0bb40
William So <polyipseity@gmail.com>
2024-03-10 20:38:12 +0800

    Move code manipulating the database to `scheme.py`
    This also enforces database invariants more easily and fixes bugs during initial indexing.
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/crawl/main.py
+++ src/egod_search/crawl/main.py
@@ -1,13 +1,11 @@
 # -*- coding: UTF-8 -*-
 from asyncio import gather
-from bisect import bisect_right
 from datetime import datetime
 from itertools import islice
 from anyio import Path
 from argparse import ArgumentParser, Namespace
 from asyncstdlib import enumerate as aenumerate
 from functools import wraps
-from re import compile
 from sys import modules
 from typing import Callable
 from bs4 import BeautifulSoup
@@ -19,19 +17,13 @@ from ..database import Database
 from ..scheme import Scheme
 from ..types import (
     Timestamp,
-    URLID_gen,
     URLStr,
-    Word,
-    WordFrequency,
-    WordID_gen,
-    WordPosition,
 )
 
 _STARTING_PAGE = URL("https://cse.hkust.edu.hk/~kwtleung/COMP4321/testpage.htm")
 _DATABASE_PATH = Path("database.json")
 _RESULT_PATH = Path("spider_result.txt")
 _NUMBER_OF_PAGES = 30
-_WORD_REGEX = compile(r"[a-zA-Z0-9\-_]+")
 
 
 async def main() -> None:
@@ -44,16 +36,17 @@ async def main() -> None:
             pass
     except FileExistsError:
         pass
-    async with await _DATABASE_PATH.open("r+t") as database_file:
+
+    async with await _DATABASE_PATH.open("r+t") as database_file, Crawler() as crawler:
         database = Database(database_file)
         try:
-            database_obj = await database.read()
+            typed_database = await database.read()
         except Database.InvalidFormat:
             await database.clear()
-            database_obj: object = {}
-        database_obj = Scheme.init(database_obj)
+            typed_database: object = {}
+        typed_database = Scheme(Scheme.init(typed_database))
 
-        async with Crawler() as crawler:
+        with typed_database.lock() as typed_database_val:
 
             async def crawl_ok_responses():
                 await crawler.enqueue(_STARTING_PAGE)
@@ -78,19 +71,8 @@ async def main() -> None:
             ):
                 if pages_indexed >= _NUMBER_OF_PAGES:
                     break
+
                 url_str = URLStr(response.url)
-                url_id = database_obj["url_ids"].setdefault(url_str, URLID_gen())
-                page = database_obj["pages"].setdefault(
-                    url_id,
-                    Scheme.Page(
-                        {
-                            "title": "",
-                            "text": "",
-                            "links": [],
-                            "mod_time": None,
-                        }
-                    ),
-                )
                 try:
                     mod_time = Timestamp(
                         int(
@@ -102,71 +84,51 @@ async def main() -> None:
                     )
                 except ValueError:
                     mod_time = None
-                if (
-                    mod_time is not None
-                    and page["mod_time"] is not None
-                    and mod_time <= page["mod_time"]
-                ):
-                    continue
                 html = BeautifulSoup(await response.text(), "html.parser")
-                page.update(
-                    {
-                        "title": "" if html.title is None else html.title.string or "",
-                        "text": html.text,
-                        "links": list(map(URLStr, outbound_urls)),
-                        "mod_time": mod_time,
-                    }
+
+                typed_database.index_page(
+                    typed_database.url_id(url_str),
+                    Scheme.Page(
+                        {
+                            "title": (
+                                "" if html.title is None else html.title.string or ""
+                            ),
+                            "text": html.text,
+                            "links": list(map(URLStr, outbound_urls)),
+                            "mod_time": mod_time,
+                        }
+                    ),
                 )
-                forward_index_page = database_obj["forward_index"].setdefault(
-                    url_id, {}
-                )
-                for match in _WORD_REGEX.finditer(html.text):
-                    position, word = WordPosition(match.start()), Word(match[0])
-                    word_id = database_obj["word_ids"].setdefault(word, WordID_gen())
 
-                    inverted_index_word_page = (
-                        database_obj["inverted_index"]
-                        .setdefault(word_id, {})
-                        .setdefault(url_id, [])
-                    )
-                    insert_index = bisect_right(inverted_index_word_page, position)
-                    if (
-                        insert_index >= 1
-                        and inverted_index_word_page[insert_index - 1] == position
-                    ):
-                        continue
-
-                    inverted_index_word_page.insert(insert_index, position)
-                    forward_index_page[word_id] = WordFrequency(
-                        forward_index_page.setdefault(word_id, WordFrequency(0)) + 1
-                    )
-
-        await database.write(database_obj)
+            await database.write(typed_database_val)
 
     async with await _RESULT_PATH.open("wt") as result_file:
-        for url_id, page in database_obj["pages"].items():
-            await result_file.write(page["title"] or "(no title)")
-            await result_file.write("\n")
-            await result_file.write(database_obj["urls"][url_id])
-            await result_file.write("\n")
-            await result_file.write(str(page["mod_time"] or "(no modification time)"))
-            await result_file.write(", ")
-            await result_file.write(
-                str(len(page["text"])) if page["text"] else "(no text)"
-            )
-            await result_file.write("\n")
-            for word_id, frequency in islice(
-                database_obj["forward_index"][url_id].items(), 10
-            ):
-                await result_file.write(database_obj["words"][word_id])
-                await result_file.write(" ")
-                await result_file.write(str(frequency))
-                await result_file.write("; ")
-            await result_file.write("\n")
-            for link in islice(page["links"], 10):
-                await result_file.write(link)
+        with typed_database.lock() as typed_database_val:
+            for url_id, page in typed_database_val["pages"].items():
+                await result_file.write(page["title"] or "(no title)")
                 await result_file.write("\n")
-            await result_file.write("\n")
+                await result_file.write(typed_database_val["urls"][url_id])
+                await result_file.write("\n")
+                await result_file.write(
+                    str(page["mod_time"] or "(no modification time)")
+                )
+                await result_file.write(", ")
+                await result_file.write(
+                    str(len(page["text"])) if page["text"] else "(no text)"
+                )
+                await result_file.write("\n")
+                for word_id, frequency in islice(
+                    typed_database_val["forward_index"][url_id].items(), 10
+                ):
+                    await result_file.write(typed_database_val["words"][word_id])
+                    await result_file.write(" ")
+                    await result_file.write(str(frequency))
+                    await result_file.write("; ")
+                await result_file.write("\n")
+                for link in islice(page["links"], 10):
+                    await result_file.write(link)
+                    await result_file.write("\n")
+                await result_file.write("\n")
 
 
 def parser(parent: Callable[..., ArgumentParser] | None = None) -> ArgumentParser:
