074201fbd41b4f03922c4b85993656f515f08be7
William So <polyipseity@gmail.com>
2024-03-28 14:04:44 +0800

    Setup `unittest` testing
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ src/egod_search/_test.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+from unittest import main as test_main
+
+
+def main() -> None:
+    """
+    Test this module.
+    """
+    test_main(module=None)
+
+
+if __name__ == "__main__":
+    main()
