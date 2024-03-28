c362ea161fb02ff131ee3ec2ba0736e4c9ac1343
William So <polyipseity@gmail.com>
2024-03-28 15:20:22 +0800

    Remove unused things in `_util.py`
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/_util.py
+++ src/egod_search/_util.py
@@ -1,11 +1,10 @@
 # -*- coding: UTF-8 -*-
 from datetime import datetime
 from aiosqlite import Connection
-from typing import Any, Iterator, Protocol, TypeVar
+from typing import Any, Protocol, TypeVar
 
 _AnyStr_co = TypeVar("_AnyStr_co", str, bytes, covariant=True)
 _AnyStr_contra = TypeVar("_AnyStr_contra", str, bytes, contravariant=True)
-_T = TypeVar("_T")
 
 _HTTP_LAST_MODIFIED = {
     "Jan": "01",
@@ -60,36 +59,6 @@ async def a_fetch_value(conn: Connection, *args: Any, default: Any = None) -> An
     return default if ret is None else ret[0]
 
 
-def getitem_or_def(obj: object, key: object, default: object = ...) -> object:
-    """
-    Return `obj[key]` if possible, otherwise `default`.
-    """
-    try:
-        return obj[key]  # type: ignore
-    except Exception:
-        return default
-
-
-def int_or_def(obj: object, default: _T = ...) -> int | _T:
-    """
-    Convert `obj` into an `int` if possible, otherwise `default`.
-    """
-    try:
-        return int(str_or_repr(obj))
-    except (TypeError, ValueError):
-        return default
-
-
-def iter_or_def(obj: object, default: Iterator[object] = iter(())) -> Iterator[object]:
-    """
-    Get the iterator of `obj` if possible, otherwise `default`.
-    """
-    try:
-        return iter(obj)  # type: ignore
-    except Exception:
-        return default
-
-
 def parse_http_last_modified(val: str) -> datetime:
     val = val[5:]
     for m_key, m_val in _HTTP_LAST_MODIFIED.items():
@@ -99,10 +68,3 @@ def parse_http_last_modified(val: str) -> datetime:
     return datetime.strptime(
         val.replace("GMT", "+0000"), "%d %m %Y %H:%M:%S %z"
     )  # `%Z` does not work, see https://bugs.python.org/issue22377`
-
-
-def str_or_repr(obj: object) -> str:
-    """
-    Return `obj` as is if it is a string, otherwise `repr(obj)`.
-    """
-    return obj if isinstance(obj, str) else repr(obj)
