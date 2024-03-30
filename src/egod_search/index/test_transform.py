6b53c8b0c3d96fa393915e8da108b4a7b2a47976
William So <polyipseity@gmail.com>
2024-03-30 13:29:10 +0800

    Fix a test case
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/index/test_transform.py
+++ src/egod_search/index/test_transform.py
@@ -17,7 +17,7 @@ class WordTestCase(TestCase):
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.": "loremipsumdolorsitametconsecteturadipiscingelitseddoeiusmodtemporincididuntutlaboreetdoloremagnaaliquautenimadminimveniamquisnostrudexercitationullamcolaborisnisiutaliquipexeacommodoconsequatduisauteiruredolorinreprehenderitinvoluptatevelitessecillumdoloreeufugiatnullapariaturexcepteursintoccaecatcupidatatnonproidentsuntinculpaquiofficiadeseruntmollitanimidestlaborum",
             "".join(
                 map(chr, range(256))
-            ): "0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzª²³µ¹º¼½¾àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ",
+            ): "0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyza23μ1o141234aaaaaaæceeeeiiiiðnoooooøuuuuyþßaaaaaaæceeeeiiiiðnoooooøuuuuyþy",
         }.items():
             self.assertEqual(output, normalize_text_for_search(input))
 
