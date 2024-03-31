d12a088aaaae591c129dcff13968a71493d52851
unknown <evanchan040511@gmail.com>
2024-04-01 03:42:04 +0800

    Sort of working

--- src/egod_search/web/main.py
+++ src/egod_search/web/main.py
@@ -9,6 +9,8 @@ from functools import wraps
 from typing import Callable
 from nicegui import ui
 from pathlib import Path as SyncPath
+import aiosqlite
+import asyncio
 
 try:
     from .. import VERSION
@@ -19,12 +21,31 @@ _CONFIGURATION_PATH = Path("web_args.json")
 _PROGRAM = __package__ or __name__
 _LOGGER = getLogger(_PROGRAM)
 
+db = None
 
-def main(*, database_path: Path) -> None:
+@ui.page('/other_page')
+async def other_page():
+    ui.label('Welcome to the other side')
+
+@ui.page('/dark_page', dark=True)
+async def dark_page():
+    ui.label('Welcome to the dark side')
+
+@ui.page('/')
+async def dark_page():
+    ui.label('Welcome to home')
+    assert db is not None
+    async with db.cursor() as cursor:
+        async with db.execute("SELECT rowid FROM main.urls WHERE content = https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm") as cursor:
+            rows = await cursor.fetchall()
+            ui.label(str(rows))
+
+
+async def main(*, database_path: Path) -> None:
     """
     Main program.
     """
-
+    global db
     basicConfig(level=INFO)
     _LOGGER.info(
         f"""arguments: {({
@@ -32,8 +53,7 @@ def main(*, database_path: Path) -> None:
         })}"""
     )
 
-    ui.label(f"version: {VERSION}")
-    ui.label(f"database_path: {database_path}")
+    db = await aiosqlite.connect(database_path)
     ui.run()
 
 
@@ -93,6 +113,6 @@ if __name__ in {"__main__", "__mp_main__"}:
     with SyncPath(_CONFIGURATION_PATH).open("rt") as config_file:
         config = load(config_file)
     VERSION = config["VERSION"]  # type: ignore
-    main(
+    asyncio.run(main(
         database_path=Path(config["database_path"]),
-    )
+    ))
