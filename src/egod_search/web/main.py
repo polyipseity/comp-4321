c0e8b9863b53ce099784c75ce2200de0603b7c2b
William So <polyipseity@gmail.com>
2024-04-01 11:51:37 +0800

    Improve `web/main.py`
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/web/main.py
+++ src/egod_search/web/main.py
@@ -2,50 +2,26 @@
 from json import dumps, load
 from logging import INFO, basicConfig, getLogger
 from sys import executable, stderr, stdin, stdout
+from aiosqlite import Connection, connect
 from anyio import Path
 from asyncio.subprocess import create_subprocess_exec
 from argparse import ArgumentParser, Namespace
 from functools import wraps
 from typing import Callable
-from nicegui import ui
+from nicegui import app, ui
 from pathlib import Path as SyncPath
-import aiosqlite
-import asyncio
 
-try:
-    from .. import VERSION
-except ImportError:
-    VERSION = "Lorem ipsum"  # type: ignore
+from egod_search import VERSION
 
 _CONFIGURATION_PATH = Path("web_args.json")
 _PROGRAM = __package__ or __name__
 _LOGGER = getLogger(_PROGRAM)
 
-db = None
 
-@ui.page('/other_page')
-async def other_page():
-    ui.label('Welcome to the other side')
-
-@ui.page('/dark_page', dark=True)
-async def dark_page():
-    ui.label('Welcome to the dark side')
-
-@ui.page('/')
-async def dark_page():
-    ui.label('Welcome to home')
-    assert db is not None
-    async with db.cursor() as cursor:
-        async with db.execute("SELECT rowid FROM main.urls WHERE content = https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm") as cursor:
-            rows = await cursor.fetchall()
-            ui.label(str(rows))
-
-
-async def main(*, database_path: Path) -> None:
+def main(*, database_path: Path) -> None:
     """
     Main program.
     """
-    global db
     basicConfig(level=INFO)
     _LOGGER.info(
         f"""arguments: {({
@@ -53,7 +29,35 @@ async def main(*, database_path: Path) -> None:
         })}"""
     )
 
-    db = await aiosqlite.connect(database_path)
+    db: Connection
+
+    async def on_startup():
+        nonlocal db
+        db = await connect(database_path.__fspath__())
+
+    async def on_shutdown():
+        await db.close()
+
+    @ui.page("/other_page")
+    async def other_page():  # type: ignore
+        ui.label("Welcome to the other side")
+
+    @ui.page("/dark_page", dark=True)
+    async def dark_page():  # type: ignore
+        ui.label("Welcome to the dark side")
+
+    @ui.page("/")
+    async def index():  # type: ignore
+        ui.label("Welcome to home")
+        async with db.cursor() as cursor:
+            async with db.execute(
+                "SELECT rowid FROM main.urls WHERE content = 'https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm'"
+            ) as cursor:
+                rows = await cursor.fetchall()
+                ui.label(str(rows))
+
+    app.on_startup(on_startup)  # type: ignore
+    app.on_shutdown(on_shutdown)  # type: ignore
     ui.run()
 
 
@@ -88,7 +92,6 @@ def parser(parent: Callable[..., ArgumentParser] | None = None) -> ArgumentParse
         await _CONFIGURATION_PATH.write_text(
             dumps(
                 {
-                    "VERSION": VERSION,
                     "database_path": (await args.database_path.resolve()).__fspath__(),
                 },
             )
@@ -112,7 +115,6 @@ def parser(parent: Callable[..., ArgumentParser] | None = None) -> ArgumentParse
 if __name__ in {"__main__", "__mp_main__"}:
     with SyncPath(_CONFIGURATION_PATH).open("rt") as config_file:
         config = load(config_file)
-    VERSION = config["VERSION"]  # type: ignore
-    asyncio.run(main(
+    main(
         database_path=Path(config["database_path"]),
-    ))
+    )
