ba95abb065423808b56f7ae4aa28fa36f0e8d506
William So <polyipseity@gmail.com>
2024-05-10 00:48:22 +0800

    Format `main.py`
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/web/main.py
+++ src/egod_search/web/main.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
-# macOS packaging support: https://nicegui.io/documentation/section_configuration_deployment#macos_packaging
+# macOS packaging support: https://nicegui.io/documentation/section_configuration_deployment#macos_packaging #
 from multiprocessing import freeze_support  # noqa
+
 freeze_support()  # noqa
 
 from anyio import Path
@@ -18,6 +19,7 @@ from typing import Callable
 
 _CONFIGURATION_PATH = Path("web_args.json")
 _PROGRAM = __package__ or __name__
+
 
 def parser(parent: Callable[..., ArgumentParser] | None = None) -> ArgumentParser:
     """
@@ -68,14 +70,16 @@ def parser(parent: Callable[..., ArgumentParser] | None = None) -> ArgumentParse
     parser.set_defaults(invoke=invoke)
     return parser
 
+
 async def on_startup() -> None:
     """
     On server startup.
     """
-    await Tortoise.init( # type: ignore
+    await Tortoise.init(  # type: ignore
         default_config(f"sqlite{DATABASE_PATH.as_uri()[len('file'):]}"),
     )
     await Tortoise.generate_schemas()
+
 
 async def on_shutdown() -> None:
     """
@@ -83,19 +87,23 @@ async def on_shutdown() -> None:
     """
     await Tortoise.close_connections()
 
+
 def layout(title: str) -> None:
     """
     Global page layout.
     """
     ui.page_title(f"{title} | E-God Search")
     with ui.header():
-        ui.button(on_click=lambda: left_drawer.toggle(),icon="menu")
+        ui.button(on_click=lambda: left_drawer.toggle(), icon="menu")
         ui.label("E-God Search").tailwind.font_size("4xl").font_weight("light")
         """with ui.menu():
             ui.menu_item("Home", on_click=lambda:ui.navigate.to("/"), auto_close=True)"""
-    with ui.left_drawer(value=False, fixed=False, bordered=True, elevated=True) as left_drawer:
-        ui.button("Home", on_click=lambda:ui.navigate.to("/"))
-        ui.button("Search", on_click=lambda:ui.navigate.to("/search"))
+    with ui.left_drawer(
+        value=False, fixed=False, bordered=True, elevated=True
+    ) as left_drawer:
+        ui.button("Home", on_click=lambda: ui.navigate.to("/"))
+        ui.button("Search", on_click=lambda: ui.navigate.to("/search"))
+
 
 @ui.page("/")
 def index():
@@ -104,6 +112,7 @@ def index():
     """
     layout("Home")
 
+
 if __name__ in {"__main__", "__mp_main__"}:
     with SyncPath(_CONFIGURATION_PATH).open("rt") as config_file:
         config = load(config_file)
@@ -111,11 +120,14 @@ if __name__ in {"__main__", "__mp_main__"}:
     basicConfig(level=INFO)
     LOGGER = getLogger(_PROGRAM)
     tab_character = "\t"
-    LOGGER.info(f"config: {dumps(config, ensure_ascii=False, indent=tab_character, sort_keys=True)}")
+    LOGGER.info(
+        f"config: {dumps(config, ensure_ascii=False, indent=tab_character, sort_keys=True)}"
+    )
 
     DATABASE_PATH = SyncPath(config["database_path"]).resolve()
 
     app.on_startup(on_startup)  # type: ignore
     app.on_shutdown(on_shutdown)  # type: ignore
-    import _search # type: ignore
+    import _search as _search  # type: ignore
+
     ui.run()
