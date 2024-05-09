f43bfd9c5365e270f93a4c580c019e2fe9624997
William So <polyipseity@gmail.com>
2024-05-10 06:40:05 +0800

    Improve `/debug` webpage
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/web/_debug.py
+++ src/egod_search/web/_debug.py
@@ -1,18 +1,26 @@
 # -*- coding: UTF-8 -*-
 import builtins
 from functools import wraps
-from io import StringIO
-from traceback import format_exception
+from inspect import isawaitable
+from traceback import print_exc
 from main import layout  # type: ignore
 from nicegui import ui
 
 
 @ui.refreshable
-def eval_code(code: str) -> None:
+async def eval_code(code: str) -> None:
+    """
+    Component that evaluates code and outputs text.
+    """
     patched_builtins = builtins.__dict__.copy()
 
     old_print = patched_builtins["print"]
-    stdout = StringIO()
+
+    class Stdout:
+        def write(self, s: str, /) -> None:
+            ui.label(s)
+
+    stdout = Stdout()
 
     @wraps(old_print)
     def print(*args: object, **kwargs: object):
@@ -20,22 +28,39 @@ def eval_code(code: str) -> None:
 
     patched_builtins["print"] = print
 
+    globals_dict = {"__builtins__": patched_builtins}
     try:
-        exec(code, {"__builtins__": patched_builtins})
+        exec(code, globals_dict)
     except BaseException as exc:
-        for line in format_exception(exc):
-            ui.label(line)
+        print_exc(file=stdout)
         if not isinstance(exc, Exception):
             raise
+
+    try:
+        ret = globals_dict["__ret__"]
+    except KeyError:
+        pass
     else:
-        ui.label(stdout.getvalue())
+        if isawaitable(ret):
+            try:
+                ret = await ret
+            except BaseException as exc:
+                print_exc(file=stdout)
+                if not isinstance(exc, Exception):
+                    raise
+        stdout.write(str(ret))
 
 
 @ui.page("/debug")
-def debug() -> None:
+async def debug() -> None:
+    """
+    Debug page.
+    """
     layout("Debug")
 
     with ui.row():
         code_textarea = ui.textarea(label="Evaluate Python code")
         ui.button(icon="send", on_click=lambda: eval_code.refresh(code_textarea.value))
-    eval_code("")
+    eval_code_ret = eval_code("")
+    assert isawaitable(eval_code_ret)
+    await eval_code_ret
