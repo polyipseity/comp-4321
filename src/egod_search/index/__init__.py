f4f59499820c540f51b64b5a7e6bfbd853282701
William So <polyipseity@gmail.com>
2024-05-10 00:37:55 +0800

    Fix and improve models
    Signed-off-by: William So <polyipseity@gmail.com>

--- src/egod_search/index/__init__.py
+++ src/egod_search/index/__init__.py
@@ -3,8 +3,9 @@ from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from time import time
-from typing import Collection, Mapping, MutableSequence, Sequence
+from typing import Collection, Mapping, MutableSequence, NamedTuple, Sequence
 from bs4 import BeautifulSoup, Tag
+from numpy import amax, array, float64, int64, zeros_like
 from yarl import URL
 
 from .transform import default_transform
@@ -41,6 +42,26 @@ class IndexedPage:
     Indexed Page metadata.
     """
 
+    class WordOccurrences(NamedTuple):
+        """
+        Word occurrences metadata.
+        """
+
+        positions: Sequence[int]
+        """
+        Word positions.
+        """
+        frequency: int
+        """
+        Number of occurrences.
+        """
+        tf_normalized: float
+        """
+        Term frequency, normalized.
+
+        Calculated by (number of occurrences in the page / max number of occurrences of a word in the page).
+        """
+
     url: URL
     """
     URL of the page.
@@ -69,13 +90,17 @@ class IndexedPage:
     """
     Outgoing links from this page.
     """
-    word_occurrences: Mapping[str, Sequence[int]]
+    word_occurrences: Mapping[str, WordOccurrences]
     """
     Word occurrences in the plaintext.
+
+    Must not contain empty word occurrences.
     """
-    word_occurrences_title: Mapping[str, Sequence[int]]
+    word_occurrences_title: Mapping[str, WordOccurrences]
     """
     Word occurrences in the title.
+    
+    Must not contain empty word occurrences.
     """
 
 
@@ -122,6 +147,21 @@ def index_page(page: UnindexedPage) -> IndexedPage:
     for pos, word in default_transform(title):
         word_occurrences_title[word].append(pos)
 
+    word_freqs = array([len(pos) for pos in word_occurrences.values()], dtype=int64)
+    word_freqs_title = array(
+        [len(pos) for pos in word_occurrences_title.values()], dtype=int64
+    )
+    if (word_freq_max := amax(word_freqs, initial=0)) > 0:
+        word_tfs = word_freqs / word_freq_max
+    else:
+        assert word_freqs.size == 0
+        word_tfs = zeros_like(word_freqs, dtype=float64)
+    if (word_freq_max_title := amax(word_freqs_title, initial=0)) > 0:
+        word_tfs_title = word_freqs_title / word_freq_max_title
+    else:
+        assert word_freqs_title.size == 0
+        word_tfs_title = zeros_like(word_freqs_title, dtype=float64)
+
     return IndexedPage(
         url=url,
         mod_time=datetime.fromtimestamp(mod_time, timezone.utc),
@@ -130,6 +170,19 @@ def index_page(page: UnindexedPage) -> IndexedPage:
         plaintext=plaintext,
         title=title,
         links=page.links,
-        word_occurrences=word_occurrences,
-        word_occurrences_title=word_occurrences_title,
+        word_occurrences={
+            key: IndexedPage.WordOccurrences(val, frequency=freq, tf_normalized=tf)
+            for (key, val), freq, tf in zip(
+                word_occurrences.items(), word_freqs, word_tfs, strict=True
+            )
+        },
+        word_occurrences_title={
+            key: IndexedPage.WordOccurrences(val, frequency=freq, tf_normalized=tf)
+            for (key, val), freq, tf in zip(
+                word_occurrences_title.items(),
+                word_freqs_title,
+                word_tfs_title,
+                strict=True,
+            )
+        },
     )
